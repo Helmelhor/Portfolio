eff533ef5458bb826ff462ce5b9000e908048a33
helmer barcelos <snorlaxgamer17@gmail.com>
2025-07-06 16:54:42 -0300

    deletando todo o django

--- portfolio/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .dados import habilidade
-
-# Create your views here.
-def home(request):
-    return render(request, "home.html", {"habilidade": habilidade})
